667b9858076165d2eee467d374927efb17e2680f
tomyesp <tomyesposito15@gmail.com>
2023-09-16 06:58:48 -0300

    estilos en footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useContext } from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./Routes/Home"
@@ -6,6 +6,8 @@ import Detail from "./Routes/Detail"
 import Favs from "./Routes/Favs"
 import Contact from "./Routes/Contact"
 import { ContextProvider } from "./Components/utils/global.context";
+import { ContextGlobal } from "./Components/utils/global.context";
+
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 
@@ -14,21 +16,25 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 /* fede y tomy */
 
 function App() {
+
+  
+
   return (
-      <div className="App">
-        <ContextProvider>
-          <BrowserRouter>
-            <Navbar/>
-            <Routes>
-                  <Route index element={<Home/>} />
-                  <Route path="/detail/:id" element={<Detail/>} />
-                  <Route path="/contact" element={<Contact/>} />
-                  <Route path="/favs" element={<Favs/>} />
-            </Routes>
-            </BrowserRouter>           
-            <Footer/>
-        </ContextProvider>   
-      </div>
+    <ContextProvider>
+    <div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+            <Route index element={<Home/>} />
+            <Route path="/detail/:id" element={<Detail/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/favs" element={<Favs/>} />
+      </Routes>
+      </BrowserRouter>           
+      <Footer/>
+    </div>
+  </ContextProvider>   
+
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ReactDOM from 'react-dom/client';
+import { ContextGlobal } from './Components/utils/global.context';
 import './index.css';
 import App from './App';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
-  <>
+  
+  <div>
       <App/>
-  </>
+  </div>
 );
