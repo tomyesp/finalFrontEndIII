130bc348bbae63ca1e9e4b149776802f42ace38f
Fede-sa <cr.sanchezfederico@gmail.com>
2023-09-16 03:08:14 -0300

    algo de estilo

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -8,9 +8,11 @@ const Contact = () => {
   const { state } = useContext(ContextGlobal);
   return (
     <div className={state.theme}>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
-      <Form/>
+      <div className='form'>
+        <h2>Want to know more?</h2>
+        <p>Send us your questions and we will contact you</p>
+        <Form/>
+      </div>
     </div>
   )
 }
